482960879c02f21a5e3ac6612f91779089967937
Hensvik <872304091@qq.com>
2021-06-17 09:03:12 +0800

    延迟发送

---
+++ src/main/java/delay/Producer.java
@@ -0,0 +1,54 @@
+package delay;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.MessageQueueSelector;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageQueue;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class Producer {
+    public static void main(String[] args) throws Exception {
+        DefaultMQProducer producer = new DefaultMQProducer("group1");
+
+        producer.setNamesrvAddr("159.75.115.106:9876");
+
+        producer.start();
+
+        String[] tags = new String[]{"TagA", "TagC", "TagD"};
+
+        // 订单列表
+        List<OrderStep> orderList = OrderStep.buildOrders();
+
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String dateStr = sdf.format(date);
+        for (int i = 0; i < 10; i++) {
+            // 加个时间前缀
+            String body = dateStr + " Hello RocketMQ " + orderList.get(i);
+            Message msg = new Message("DelayTopic","Order", "KEY" + i, body.getBytes());
+
+            msg.setDelayTimeLevel(2);
+            SendResult sendResult = producer.send(msg, new MessageQueueSelector() {
+                @Override
+                public MessageQueue select(List<MessageQueue> mqs, Message msg, Object arg) {
+                    Long id = (Long) arg;  //根据订单id选择发送queue
+                    long index = id % mqs.size();
+                    return mqs.get((int) index);
+                }
+            }, orderList.get(i).getOrderId());//订单id
+
+            System.out.println(String.format("SendResult status:%s, queueId:%d, body:%s",
+                    sendResult.getSendStatus(),
+                    sendResult.getMessageQueue().getQueueId(),
+                    body));
+        }
+
+        producer.shutdown();
+
+    }
+
+}
